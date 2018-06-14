d6889edc5841031c09e70170e1ead756d9aab85d
goodalexander <goodalexander@gmail.com>
2018-06-14 12:24:09 -0700

    make the setup
    installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ibpandas',
+      version='0.1',
+      description='A library for trading your Interactive Brokers account with Pandas',
+      url='https://github.com/goodalexander/ibpandas',
+      author='goodalexander',
+      author_email='goodalexander@gmail.com',
+      license='MIT',
+      packages=['ibpandas'],
+      zip_safe=False)
