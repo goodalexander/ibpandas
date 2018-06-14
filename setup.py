726e90c7deeecd2741fb7c34e5a8cb1801f370c3
Coin Admin <goodalexander@gmail.com>
2018-06-14 12:51:15 -0700

    fixed setup to include reqs
    p

--- setup.py
+++ setup.py
@@ -1,11 +1,40 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(name='ibpandas',
-      version='0.1',
-      description='A library for trading your Interactive Brokers account with Pandas',
-      url='https://github.com/goodalexander/ibpandas',
-      author='goodalexander',
-      author_email='goodalexander@gmail.com',
-      license='MIT',
-      packages=['pandas','ibapi','threading','queue','time'],
-      zip_safe=False)
+import os.path
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    sys.exit()
+
+setup(
+    name='ibpandas',
+    packages=[],
+    description='ib for pandas, pandas for ib',
+    long_description='use pandas to interact w the interactive brokers api',
+    version=1,
+    author='Alex Good',
+    author_email='goodalexander@gmail.com',
+    url='https://github.com/goodalexander/ibpandas',
+    keywords=['interactive-brokers', 'ib-pandas'],
+    install_requires=['requests>=2.2.1','pandas>=0.21.1','ibapi>=9.73.6'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    license='MIT'
+    )
